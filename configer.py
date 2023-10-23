c4a6868c1f55427324db3ea00e574a5e2e046598
Brady Bangasser <bradybbangasser@gmail.com>
2023-10-22 21:06:26 -0500

    sync

--- configer.py
+++ configer.py
@@ -1,28 +1,46 @@
-import json
+import json, hashlib
 
 # generate meta
-buildId = ""
-buildDate = ""
-builtBy = ""
+buildId = "cat"
+buildDate = "the"
+builtBy = "hi"
 
 # read in the main config
 mainConfig = open("./build-config.json")
-data = json.load(mainConfig)
+data: dict = json.load(mainConfig)
 mainConfig.close()
 
-# configure the server
-serverData = {
-    "meta": {
-
-    }
+dataTemplate = {
+    "buildId": buildId,
+    "buildDate": buildDate,
+    "builtBy": builtBy,
+    "data": {}
 }
 
+# configure the server
+serverData = dataTemplate.copy()
+
+for key in data.keys():
+    serverData[key] = data[key]
+
+serverJson = json.dumps(serverData)
 
 serverConfig = open("./server/config.json", mode="w")
+serverConfig.write(serverJson)
+serverConfig.close()
+
+serverHash = hashlib.sha512(open("./server/config.json").buffer)
+
+print(serverHash)
+
+
+print(serverData, dataTemplate)
+
+
 clientConfig = open("./client/config.json", mode="w")
 
 print(data)
 
 
-serverConfig.close()
+
 clientConfig.close()

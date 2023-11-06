1955d762e02b95b2e10ff9120c342f641e42052b
Brady Bangasser <bradybbangasser@gmail.com>
2023-11-06 14:25:40 -0600

    Push

--- client/src/app/dashboard/layout-route.tsx
+++ client/src/app/dashboard/layout-route.tsx
@@ -4,28 +4,31 @@ import { SubRoute } from "./types";
 import styles from "./styles.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react"
 
 export default function LayoutRoute({ name, route }: {
     name: string
     route: string | SubRoute[]
 }): JSX.Element {
+    const [selected, setSelected] = useState(false)
     if (Array.isArray(route)) {
         return (
-            <div className={styles.nestedNavlink}>
+            <div className={`${styles.nestedNavlink} ${selected ? styles.selectedDropdown : ""}`} onClick={() => setSelected(!selected)}>
                 <div className={styles.nestedRouteTitle}>
                     <div className={styles.dropsign}>
                         <FontAwesomeIcon icon={faChevronRight} rotate={90} />
                     </div>
-                    {name}
+                    <div className={styles.droptitle}>{name}</div>
                 </div>
                 <div className={styles.dropdown}>
+                    {route.map((subroute, index) => <a key={index}>{subroute.name}</a>)}
                 </div>
             </div>
         )
     } else {
         return (
             <div className={styles.navlink}>
-                <a href={route as string}>{name}</a>
+                <div className={styles.droptitle}><a href={route as string}>{name}</a></div>
             </div>
         )
     }
